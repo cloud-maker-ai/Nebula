47c54b523bbe152f6e7794ea8ed32a3a12760eab
Joel Oughton <joel.oughton@cloudmaker.ai>
2019-06-25 21:18:19 +0100

    Added support for including an associated actor id with a document write operation.
    A default ASP.NET Core implementation for the new IDocumentMetadataSource has been added that reads the user's id from the claims on the identity from the current HTTP context.

---
+++ Nebula.AspNetCore/DocumentMetadataSource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace Nebula.AspNetCore
+{
+    /// <summary>
+    /// A standard ASP.NET Core document metadata source.
+    /// </summary>
+    public class DocumentMetadataSource : IDocumentMetadataSource
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="DocumentMetadataSource"/> class.
+        /// </summary>
+        /// <param name="httpContextAccessor">The http context accessor.</param>
+        public DocumentMetadataSource(IHttpContextAccessor httpContextAccessor)
+        {
+            if (httpContextAccessor == null)
+                throw new ArgumentNullException(nameof(httpContextAccessor));
+
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <inheritdoc />
+        public string GetActorId()
+        {
+            var claims = _httpContextAccessor.HttpContext.User.Claims.ToArray();
+
+            string actorId = null;
+
+            var subClaim =
+                claims.SingleOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+                ?? claims.SingleOrDefault(c => c.Type == "sub");
+
+            if (subClaim != null)
+            {
+                actorId = subClaim.Value;
+            }
+
+            return actorId;
+        }
+    }
+}
